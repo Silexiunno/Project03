7cc25f3425d5061ec1ce9ca23e51aaa36c9200d6
Katie <m.c.richard174@gmail.com>
2023-08-07 12:09:35 -0500

    8/7

--- Assets/_Game/Scripts/ParticleDataEditor.cs
+++ Assets/_Game/Scripts/ParticleDataEditor.cs
@@ -8,13 +8,62 @@ public class ParticleDataEditor : Editor
     private SerializedProperty _color;
     private SerializedProperty _duration;
     private SerializedProperty _speed;
+    private SerializedProperty _playonawake;
+    private SerializedProperty _startMax;
+    private SerializedProperty _startMin;
+    private SerializedProperty _particlename;
+    private SerializedProperty _default;
 
-    private void OnEnable()
+    //Color matColor = Color.white;
+
+
+    public void OnEnable()
     {
         _color = serializedObject.FindProperty("_color");
         _duration = serializedObject.FindProperty("_duration");
         _speed = serializedObject.FindProperty("_speed");
-    }
+        _playonawake = serializedObject.FindProperty("_playonawake");
+        _startMax = serializedObject.FindProperty("_startMax");
+        _startMin = serializedObject.FindProperty("_startMin");
+        _particlename = serializedObject.FindProperty("_particlename");
+        _default = serializedObject.FindProperty("_default");
 
+     }
+
+
+    public override void OnInspectorGUI()
+    {
+        //  base.OnInspectorGUI();
+        EditorGUILayout.LabelField("Default Particle", EditorStyles.boldLabel);
+        EditorGUILayout.PropertyField(_default, new GUIContent("Particle Prefab", "Place default particle here please!"));
+        EditorGUILayout.Space(15);
+
+
+        EditorGUILayout.LabelField("General Stats", EditorStyles.boldLabel);
+        EditorGUILayout.PropertyField(_particlename, new GUIContent("Name"));
+        if (_particlename.stringValue == string.Empty)
+        {
+            EditorGUILayout.HelpBox
+                ("Please give the Particle a name",
+                MessageType.Warning);
+        }
+      
+
+      //  matColor = EditorGUILayout.ColorField("New Color", matColor);
+
+
+
+        EditorGUILayout.Space(15);
+        ParticlesData data = (ParticlesData)target;
+        if (GUILayout.Button("Create"))
+        {
+            data.Create();
+
+        }
+    }
+   
+    
+   
+    
 }
     
--- Assets/_Game/Scripts/ParticlesData.cs
+++ Assets/_Game/Scripts/ParticlesData.cs
@@ -5,17 +5,43 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Particle", fileName = "P_")]
 public class ParticlesData : ScriptableObject
 {
-    //stuff we want to customize for this 
-    // Color
-    // Duration 
-    // Speed 
-
-    [SerializeField] private Color _color = Color.red;
+  
+    [SerializeField] private Color _color = Color.red; 
     [SerializeField] private float _duration = 3f;
     [SerializeField] private float _speed = 0.3f;
-    
+    [SerializeField] private bool _playonawake;
+    [SerializeField] private float _startMax;
+    [SerializeField] private float _startMin;
+    [SerializeField] private string _particlename;
+
+    [SerializeField] public ParticleSystem _default;
+   
+
+
+
+    public void Create()
+    {
+        InstantiatePrefab(_default);
+    }
+    public void InstantiatePrefab(ParticleSystem P_Default)
+    {
+        ParticleSystem newsystem = Instantiate(P_Default);
+        newsystem.gameObject.name = _particlename;
+        var newMainsystem = newsystem.main;
+        newMainsystem.startSpeed = _speed;
+        newMainsystem.duration = _duration;
+        newMainsystem.playOnAwake = _playonawake;
+        newMainsystem.startSize = Random.Range(_startMin, _startMax);
+
+
+    }
+
     public float Duration => _duration;
     public float Speed => _speed;
+    public bool PlayonAwake => _playonawake;
+    public float StartMax => _startMax;
+    public float StartMin => _startMin;
     public Color Color => _color;
-
+    public string ParticleName => _particlename;
+    
 }
